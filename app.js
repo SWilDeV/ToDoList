58a0d7d868c5ce99c04798d8cd9fb0b10380ffb6
sean wilson <sean0669@gmail.com>
2021-06-05 18:02:43 -0400

    cors6

--- app.js
+++ app.js
@@ -19,14 +19,6 @@ mongoose.connect(dbUrl, {
   useUnifiedTopology: true,
 });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin:*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(
   cors({
