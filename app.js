64cc27d28e9c071b8c67fee3c6b84a035587e1c0
sean wilson <sean0669@gmail.com>
2021-06-05 17:50:42 -0400

    cors3

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ mongoose.connect(dbUrl, {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://peaceful-dusk-66565.herokuapp.com"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
