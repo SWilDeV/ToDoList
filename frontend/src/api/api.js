9d27fe6ed65cd3fd2bdb0afbccc4b1a20f7ffff0
sean wilson <sean0669@gmail.com>
2021-06-05 18:55:46 -0400

    static readded

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ export default class Api {
     this.baseURL = `/`;
     //  this.baseURL = `https://peaceful-dusk-66565.herokuapp.com`;
 
-    // this.baseURL = `http://localhost:5000`;
+    // this.baseURL  = `http://localhost:5000`;
   }
 
   registerUser = (user) => {
