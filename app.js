99434486a624c2ae4e8d65cda9b63c2e5be17cda
sean wilson <sean0669@gmail.com>
2021-06-05 17:08:32 -0400

    CORS handling3

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ mongoose.connect(dbUrl, {
 });
 
 app.use(
-  function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-    );
-  },
+  // function (req, res) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Credentials", true);
+  //   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  //   );
+  // },
   cors({
     origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
